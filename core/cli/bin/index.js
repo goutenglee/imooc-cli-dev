0e87855850cc7b2261182428f40fbb97fa563426
Gouteng <603217548@qq.com>
2022-03-07 17:22:31 +0800

    fix

--- core/cli/bin/index.js
+++ core/cli/bin/index.js
@@ -5,5 +5,5 @@ const importLocal = require("import-local");
 if (importLocal(__filename)) {
   require("npmlog").info("cli", "正在使用本地 imooc-cli-beta");
 } else {
-  require("../lib/core.js")(process.argv.slice(2));
+  require("../lib/index.js")(process.argv.slice(2));
 }
